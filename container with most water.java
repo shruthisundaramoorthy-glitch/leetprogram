68ceb64c4b56e5b86244d512b450a560dd5fd7ae
Shruthisundaramoorthy <shruthisundaramoorthy@gmail.com>
2026-03-25 10:24:58 +0530

    Add files via upload

---
+++ container with most water.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int max = 0;
+        while (left < right) {
+            int h = Math.min(height[left], height[right]);
+            int area = h * (right - left);
+            if (area > max) max = area;
+            if (height[left] < height[right]) left++;
+            else right--;
+        }
+        return max;
+    }
+}
