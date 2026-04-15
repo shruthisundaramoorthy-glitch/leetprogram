2bf604831d302aedbc9edce93d253e40216315b3
Shruthisundaramoorthy <shruthisundaramoorthy@gmail.com>
2026-04-15 12:19:51 +0530

    Add files via upload

---
+++ Largest number.java
@@ -0,0 +1,34 @@
+
+
+public class Solution {
+    public String largestNumber(int[] nums) {
+        
+        String[] strNums = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            strNums[i] = String.valueOf(nums[i]);
+        }
+
+    
+        Arrays.sort(strNums, new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                
+                return (b + a).compareTo(a + b);
+            }
+        });
+
+        if (strNums[0].equals("0")) {
+            return "0";
+        }
+
+        
+        StringBuilder largestNumber = new StringBuilder();
+        for (String str : strNums) {
+            largestNumber.append(str);
+        }
+
+        return largestNumber.toString();
+    }
+
+    
+}
